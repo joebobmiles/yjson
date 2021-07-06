589e8b1a71f181b967a1b503f8b9b98000ea1dfb
Joseph R Miles <joe@jrm.dev>
2021-07-06 12:51:06 -0700

    feat: added better types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,10 +1,12 @@
 import Y from "yjs";
 
-export const yjson:
-  <T = { [key: string]: any }>(doc: Y.Doc, name?: string) =>
-  T &
-  {
-    observe: (callback: (events: Array<Y.YEvent>, transaction: Y.Transaction) => void) => void,
-  };
+interface IYJSON
+{
+  observe: (callback: (events: Array<Y.YEvent>, transaction: Y.Transaction) => void) => void;
+}
+
+export type YJSON<T = { [key: string]: any }> = IYJSON & T;
+
+export const yjson: <T = { [key: string]: any }>(doc: Y.Doc, name?: string) => YJSON<T>;
 
 export default yjson;
