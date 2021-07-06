cc1dff2f610ade9fa15bc4ae56ca859d0ee9dec9
Joseph R Miles <joe@jrm.dev>
2021-07-06 07:04:26 -0700

    refactor: moved original tests into a describe block

--- src/index.spec.js
+++ src/index.spec.js
@@ -3,128 +3,130 @@ const yjson = require(".").default;
 
 describe("yjson", () =>
 {
-  let doc1;
-  let doc2;
-
-  beforeEach(() =>
+  describe("When used to create two linked stores", () =>
   {
-    doc1 = new Y.Doc();
-    doc2 = new Y.Doc();
+    let doc1;
+    let doc2;
 
-    doc1.on("update", (update) =>
+    beforeEach(() =>
     {
-      Y.applyUpdate(doc2, update);
+      doc1 = new Y.Doc();
+      doc2 = new Y.Doc();
+
+      doc1.on("update", (update) =>
+      {
+        Y.applyUpdate(doc2, update);
+      });
+      doc2.on("update", (update) =>
+      {
+        Y.applyUpdate(doc1, update);
+      });
     });
-    doc2.on("update", (update) =>
+
+    it("Creates stores that share scalar values.", () =>
     {
-      Y.applyUpdate(doc1, update);
+      const objectA = yjson(doc1);
+      const objectB = yjson(doc2);
+
+      objectA.test = 3;
+
+      expect(objectA.test).toEqual(3);
+      expect(objectB.test).toEqual(3);
+    });
+
+    it("Creates stores that share arrays.", () =>
+    {
+      const objectA = yjson(doc1);
+      const objectB = yjson(doc2);
+
+      objectA.test = [ "hello", "world" ];
+
+      expect(objectA.test).toEqual([ "hello", "world" ]);
+      expect(objectB.test).toEqual([ "hello", "world" ]);
+
+      objectA.test[0] = "goodbye";
+
+      expect(objectA.test).toEqual([ "goodbye", "world" ]);
+      expect(objectB.test).toEqual([ "goodbye", "world" ]);
+    });
+
+    it("Creates stores that share nested arrays.", () =>
+    {
+      const objectA = yjson(doc1);
+      const objectB = yjson(doc2);
+
+      objectA.test = [ "hello", [ "world", "universe" ] ];
+
+      expect(objectA.test).toEqual([ "hello", [ "world", "universe" ] ]);
+      expect(objectB.test).toEqual([ "hello", [ "world", "universe" ] ]);
+
+      objectA.test[1][1] = "cosmos";
+
+      expect(objectA.test).toEqual([ "hello", [ "world", "cosmos" ] ]);
+      expect(objectB.test).toEqual([ "hello", [ "world", "cosmos" ] ]);
+    });
+
+    it("Creates stores that share objects nested in arrays.", () =>
+    {
+      const objectA = yjson(doc1);
+      const objectB = yjson(doc2);
+
+      objectA.test = [ { foo: 1 }, { bar: 2 } ];
+
+      expect(objectA.test).toEqual([ { foo: 1 }, { bar: 2 } ]);
+      expect(objectB.test).toEqual([ { foo: 1 }, { bar: 2 } ]);
+
+      objectA.test[0].foo = 3;
+
+      expect(objectA.test[0].foo).toEqual(3);
+      expect(objectB.test[0].foo).toEqual(3);
+    });
+
+    it("Creates stores that share objects.", () =>
+    {
+      const objectA = yjson(doc1);
+      const objectB = yjson(doc2);
+
+      objectA.test = { "hello": "world" };
+
+      expect(objectA.test).toEqual({ "hello": "world" });
+      expect(objectB.test).toEqual({ "hello": "world" });
+
+      objectA.test.hello = "samwise";
+
+      expect(objectA.test).toEqual({ "hello": "samwise" });
+      expect(objectB.test).toEqual({ "hello": "samwise" });
+    });
+
+    it("Creates stores that share nested objects.", () =>
+    {
+      const objectA = yjson(doc1);
+      const objectB = yjson(doc2);
+
+      objectA.test = { foo: { bar: 'baz' } };
+
+      expect(objectA.test).toEqual({ foo: { bar: 'baz' } });
+      expect(objectB.test).toEqual({ foo: { bar: 'baz' } });
+
+      objectA.test.foo.bar = 3;
+
+      expect(objectA.test.foo.bar).toEqual(3);
+      expect(objectB.test.foo.bar).toEqual(3);
+    });
+
+    it("Creates stores that share arrays nested in objects.", () =>
+    {
+      const objectA = yjson(doc1);
+      const objectB = yjson(doc2);
+
+      objectA.test = { foo: [ 'bar', 'baz' ] };
+
+      expect(objectA.test).toEqual({ foo: [ 'bar', 'baz' ] });
+      expect(objectB.test).toEqual({ foo: [ 'bar', 'baz' ] });
+
+      objectA.test.foo[0] = 3;
+
+      expect(objectA.test.foo[0]).toEqual(3);
+      expect(objectB.test.foo[0]).toEqual(3);
     });
   });
-
-  it("Creates stores that share scalar values.", () =>
-  {
-    const objectA = yjson(doc1);
-    const objectB = yjson(doc2);
-
-    objectA.test = 3;
-
-    expect(objectA.test).toEqual(3);
-    expect(objectB.test).toEqual(3);
-  });
-
-  it("Creates stores that share arrays.", () =>
-  {
-    const objectA = yjson(doc1);
-    const objectB = yjson(doc2);
-
-    objectA.test = [ "hello", "world" ];
-
-    expect(objectA.test).toEqual([ "hello", "world" ]);
-    expect(objectB.test).toEqual([ "hello", "world" ]);
-
-    objectA.test[0] = "goodbye";
-
-    expect(objectA.test).toEqual([ "goodbye", "world" ]);
-    expect(objectB.test).toEqual([ "goodbye", "world" ]);
-  });
-
-  it("Creates stores that share nested arrays.", () =>
-  {
-    const objectA = yjson(doc1);
-    const objectB = yjson(doc2);
-
-    objectA.test = [ "hello", [ "world", "universe" ] ];
-
-    expect(objectA.test).toEqual([ "hello", [ "world", "universe" ] ]);
-    expect(objectB.test).toEqual([ "hello", [ "world", "universe" ] ]);
-
-    objectA.test[1][1] = "cosmos";
-
-    expect(objectA.test).toEqual([ "hello", [ "world", "cosmos" ] ]);
-    expect(objectB.test).toEqual([ "hello", [ "world", "cosmos" ] ]);
-  });
-
-  it("Creates stores that share objects nested in arrays.", () =>
-  {
-    const objectA = yjson(doc1);
-    const objectB = yjson(doc2);
-
-    objectA.test = [ { foo: 1 }, { bar: 2 } ];
-
-    expect(objectA.test).toEqual([ { foo: 1 }, { bar: 2 } ]);
-    expect(objectB.test).toEqual([ { foo: 1 }, { bar: 2 } ]);
-
-    objectA.test[0].foo = 3;
-
-    expect(objectA.test[0].foo).toEqual(3);
-    expect(objectB.test[0].foo).toEqual(3);
-  });
-
-  it("Creates stores that share objects.", () =>
-  {
-    const objectA = yjson(doc1);
-    const objectB = yjson(doc2);
-
-    objectA.test = { "hello": "world" };
-
-    expect(objectA.test).toEqual({ "hello": "world" });
-    expect(objectB.test).toEqual({ "hello": "world" });
-
-    objectA.test.hello = "samwise";
-
-    expect(objectA.test).toEqual({ "hello": "samwise" });
-    expect(objectB.test).toEqual({ "hello": "samwise" });
-  });
-
-  it("Creates stores that share nested objects.", () =>
-  {
-    const objectA = yjson(doc1);
-    const objectB = yjson(doc2);
-
-    objectA.test = { foo: { bar: 'baz' } };
-
-    expect(objectA.test).toEqual({ foo: { bar: 'baz' } });
-    expect(objectB.test).toEqual({ foo: { bar: 'baz' } });
-
-    objectA.test.foo.bar = 3;
-
-    expect(objectA.test.foo.bar).toEqual(3);
-    expect(objectB.test.foo.bar).toEqual(3);
-  });
-
-  it("Creates stores that share arrays nested in objects.", () =>
-  {
-    const objectA = yjson(doc1);
-    const objectB = yjson(doc2);
-
-    objectA.test = { foo: [ 'bar', 'baz' ] };
-
-    expect(objectA.test).toEqual({ foo: [ 'bar', 'baz' ] });
-    expect(objectB.test).toEqual({ foo: [ 'bar', 'baz' ] });
-
-    objectA.test.foo[0] = 3;
-
-    expect(objectA.test.foo[0]).toEqual(3);
-    expect(objectB.test.foo[0]).toEqual(3);
-  });
-});
